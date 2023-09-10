8e62363bf1e3177f55cb77c237ce3132d35a02ae
hugo2lee <hugo2lee@gmail.com>
2023-09-10 23:58:53 +0800

    作业5占坑

---
+++ webook/internal/web/homework5_test.go
@@ -0,0 +1,11 @@
+/*
+ * @Author: hugo2lee hugo2lee@gmail.com
+ * @Date: 2023-09-10 23:58
+ * @LastEditors: hugo2lee hugo2lee@gmail.com
+ * @LastEditTime: 2023-09-10 23:58
+ * @FilePath: /geektime-basic-go/webook/internal/web/homework5_test.go
+ * @Description:
+ *
+ * Copyright (c) 2023 by hugo, All Rights Reserved.
+ */
+package web
